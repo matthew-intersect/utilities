a93423c4d0130ff62cf98f066105dab05cbfa165
matthew-intersect <matthew@intersect.org.au>
2013-07-30 17:15:17 +1000

    add unique artists to unique counter + fix bugs

--- lfm-uniq.py
+++ lfm-uniq.py
@@ -2,6 +2,9 @@
 import urllib2
 import re
 
+#
+# Unique tracks
+#
 html = urllib2.urlopen('http://www.last.fm/user/BurbotsRevenge/library/recent')
 library_recent = html.read()
 
@@ -23,5 +26,23 @@ finalUrl = "http://www.last.fm/user/BurbotsRevenge/library/recent?page=" + str(l
 
 html = urllib2.urlopen(finalUrl)
 lastPage = html.read()
-unique = (last*20) + lastPage.count('<tr ')
+unique = ((last-1)*20) + lastPage.count('<tr ')
 print("Unique tracks in library: " + str(unique))
+
+#
+# Unique artists
+#
+html = urllib2.urlopen('http://www.last.fm/user/BurbotsRevenge/library')
+library_recent = html.read()
+
+toFind = 'class="pagenumber">1</span>'
+index = library_recent.find(toFind)
+index = index + len(toFind) + 4
+page = int(library_recent[index:library_recent.find(" ", index)])
+
+finalUrl = 'http://www.last.fm/user/BurbotsRevenge/library?page=' + str(page)
+html = urllib2.urlopen(finalUrl)
+lastPage = html.read()
+
+unique = ((page-1)*18) + lastPage.count('class="pictureFrame"')
+print("Unique artists in library: " + str(unique))
